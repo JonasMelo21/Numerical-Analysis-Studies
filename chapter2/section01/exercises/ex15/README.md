31f1f2f046a1f3959e609d9ab202f5839509bd6d
Jonas_Melo <jonashonorato4@gmail.com>
2024-08-11 23:05:23 -0300

    Add files via upload

---
+++ chapter2/section01/exercises/ex15/README.md
@@ -0,0 +1,13 @@
+# Exercise 15
+
+![image](image.png)
+
+## Theorem
+
+![theorem](theorem.png)
+
+## Solutions
+
+Markdown solution to find upper bound of maxIteractions: [solution.md](solution.md)
+
+C Programming Language file: [solution.c](solution.c)
---
+++ chapter2/section01/exercises/ex15/ex15.c
@@ -0,0 +1,60 @@
+/*
+    Created by Jonas H. Melo on 2024/08/11
+    This file is protected under MIT license
+
+    This code is part of my studies in numerical analysis,
+    following Section 1 of Chapter 2 in the book "Numerical Analysis"
+    by Richard L. Burden and J. Douglas Faires, 11th edition.
+
+    Approach: Let's find a solution for x^3 - x - 1 = 0 with 10^(-4) digits of precision.
+*/
+
+#include <math.h>
+#include <stdio.h>
+
+double f(double x) {
+    return pow(x, 3) - x - 1;
+}
+
+double bisection(double (*func)(double), double interval[2], double tol, int maxIterations) {
+    double a = interval[0];
+    double b = interval[1];
+    double FA = func(a);
+    double p;
+    double FP;
+    int i = 1;
+
+    while (i <= maxIterations) {
+        p = a + (b - a) / 2;
+        FP = func(p);
+
+        if (FP == 0 || (b - a) < tol) {
+            printf("Procedure executed successfully after %d iterations\n", i);
+            return p;
+        } else if (FA * FP > 0) {
+            a = p;
+            FA = FP;
+        } else {
+            b = p;
+        }
+        i++;
+    }
+
+    if (i > maxIterations) {
+        printf("\nProcedure failed after %d iterations\n", i);
+        printf("Value of p found so far: %.3f\n", p);
+        return -1;
+    }
+
+    return -1;  // Return statement added to handle all control paths
+}
+
+int main() {
+    double interval[2] = {1, 2};  // Correct initialization of the interval
+    double tol = pow(10, -3);
+    int maxIterations = 50;
+    double solution = bisection(f, interval, tol, maxIterations);
+    printf("\nSolution for the equation x^3 + x - 4: %.3f\n", solution);
+
+    return 0;
+}
